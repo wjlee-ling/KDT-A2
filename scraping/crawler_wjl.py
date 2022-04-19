0d442d94be10b77975c63abdf4f9381bac212c04
wjlee <wjlee.ling@gmail.com>
2022-04-19 17:48:22 +0900

    fix crawler_wjl: search news.naver only

--- scraping/crawler_wjl.py
+++ scraping/crawler_wjl.py
@@ -3,13 +3,15 @@
 네이버 뉴스- 조선일보 코드 : news_office_checked=1023
 
 --TO-DO:
-1. 기사 중간에 삽입된 사진과 캡션 제거 필요
+1. 검색할 신문사가 두개 이상일 때 dataframe을 나눠서 return
 ref:
 1. https://developers.naver.com/docs/search/blog/
 2. 공식API 사용할 때 (https://ysyblog.tistory.com/49)
 3. API없이 읽을 때 (https://wonhwa.tistory.com/8)
 4. https://butnotforme.tistory.com/entry/python%EC%9C%BC%EB%A1%9C-%EC%97%85%EB%AC%B4-%EC%9E%90%EB%8F%99%ED%99%94%EA%B9%8C%EC%A7%80-8-requests3?category=932590
 '''
+import re
+from tqdm import tqdm
 import requests as req
 import pandas as pd
 from time import sleep
@@ -38,18 +40,22 @@ def read_article(url, driver):
     
     driver.get(url)
     title = driver.find_element(by=By.CSS_SELECTOR, value="#articleTitle").text.strip()
-    body = driver.find_element(by=By.CSS_SELECTOR, value='#articleBodyContents').text.strip()
+    body = driver.find_element(by=By.CSS_SELECTOR, value='#articleBodyContents').text
+    try:
+        caption = driver.find_element(by=By.CSS_SELECTOR, value='.img_desc').text
+        body = body.replace(caption, '').strip() # removing image captions
+    except:
+        body = body.strip()
     written_at = driver.find_element(by=By.CSS_SELECTOR, value='#main_content > div.article_header > div.article_info > div > span.t11').text.split()[0]
     #title = driver.find_element_by_css_selector("#articleTitle").text
 
     return title, body, written_at 
 
 def search_opinions(publisher, start=1, since=None, to=None):
-    import re
     """
     언론사별 사설을 검색하여, 이들의 네이버뉴스 링크를 리턴하는 메소드
     - publisher: 언론사
-    - start: 검색 page index
+    - start: 검색 page index (display=1 일때 기준임. default가 10이므로 url상 2페이지는 21, 3페이지는 31로 )
     - since/to: 'YYYY.MM.DD' 검색할 기사 작성 날짜 기간. since가 to보다 이전이여야 함
 
     e.g.https://search.naver.com/search.naver?where=news&sm=tab_pge&query=사설&mynews=1&office_type=1&office_section_code=1&news_office_checked=1023&start=1
@@ -63,9 +69,9 @@ def search_opinions(publisher, start=1, since=None, to=None):
     if since is not None or to is not None:
         url_per_publisher = f'https://search.naver.com/search.naver?where=news&sm=tab_opt&query=사설&ds={to}&de={since}&mynews=1&office_type=1&office_section_code=1&news_office_checked={publisher_id}&start={start}&nso=so%3Ar%2Cp%3Afrom{since.replace(".","")}to{to.replace(".","")}'
         date_pattern = r'(19|20)[0-9]{2}\.[0-9]{2}\.[0-9]{2}' # 완벽하지 않음
-        if re.fullmatch(date_pattern, since) and re.fullmatch(date_pattern, since).group(0) != since: 
+        if not re.fullmatch(date_pattern, since): 
             raise Exception("잘못된 since 날짜 형식입니다.")
-        if re.fullmatch(date_pattern, to) and re.fullmatch(date_pattern, to).group(0) != to:
+        if not re.fullmatch(date_pattern, to):
             raise Exception("잘못된 to 날짜 형식입니다.")
     else:
         url_per_publisher = f'https://search.naver.com/search.naver?where=news&sm=tab_pge&query=사설&mynews=1&office_type=1&office_section_code=1&news_office_checked={publisher_id}&start={start}'
@@ -75,7 +81,7 @@ def search_opinions(publisher, start=1, since=None, to=None):
     articles = soup.find_all('a', class_=["info"])
     links = []
     for a in articles:
-        if len(a.get('class')) == 1: # 네이버 기사와 원신문사 기사 링크 중 네이버 기사 링크 선택
+        if len(a.get('class')) == 1 and 'news.naver.com' in a: # 네이버 기사와 원신문사 기사 링크 중 네이버 기사 링크 선택 & 연예/스포츠 기사 제거
             links.append(a.get('href'))
     return links
 
@@ -87,24 +93,29 @@ def scraper(publishers, pages_n=1, since=None, to=None):
     - since/to: 'YYYY.MM.DD' 검색할 기사 작성 날짜 기간. since가 to보다 이전이여야 함
     '''
     driver = webdriver.Chrome()
-    total_df = pd.DataFrame()
+    dfs = []
     for publisher in publishers:
         titles, bodies, written_ats= [],[],[]
-        for idx in range(1, pages_n+1):
-            links = search_opinions(publisher, start=idx, since=since, to=to)
+        for idx in tqdm(range(1, pages_n+1)):
+            page_idx = 10*(idx-1) + 1 # url상 검색 1페이지는 1, 2페이지는 11, 3페이지는 21,.. 10페이지는 91, 11페이지는, 101 
+            links = search_opinions(publisher, start=page_idx, since=since, to=to)
             for link in links:
-                title, body, written_at  = read_article(link, driver)
-                sleep(0.2)
+                try:
+                    title, body, written_at  = read_article(link, driver)
+                    sleep(0.15)
+                except:
+                    continue
                 if is_opinion(title):
                     titles.append(title)
                     bodies.append(body)
                     written_ats.append(written_at)
         publisher_ls = [publisher] * len(titles)
         new_df = pd.DataFrame({'newspaper': publisher_ls, 'title': titles, 'body': bodies, 'written_at': written_ats})
-        total_df = pd.concat([total_df, new_df])
+        dfs.append(new_df)
     driver.close()
-    return total_df
+    return dfs
 
-newspaper = [] # 
-corpus = scraper(newspaper, pages_n=100)
-corpus.to_csv(f'corpus_{newspaper}.csv', encoding='utf-8-sig')
+newspaper = ['조선일보', '경향신문'] 
+corpora = scraper(newspaper, pages_n=1000)
+for paper, corpus in zip(newspaper, corpora):
+    corpus.to_csv(f'corpus_{paper}.csv', encoding='utf-8-sig')
