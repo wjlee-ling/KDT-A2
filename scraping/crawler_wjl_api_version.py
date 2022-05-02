0f79cc1df91b513df1298a5a35872894e0a4d495
wjlee <wjlee.ling@gmail.com>
2022-05-02 11:01:32 +0900

    update crawler_wjl_api_version

--- scraping/crawler_wjl_api_version.py
+++ scraping/crawler_wjl_api_version.py
@@ -13,63 +13,94 @@ ref:
 import requests as req
 import pandas as pd
 import time
-from bs4 import BeautifulSoup
-from wjl_personal import naver_id, naver_pwd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 headers= {
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
-    "X-Naver-Client-Id": naver_id,
-    "X-Naver-Client-Secret": naver_pwd
+    "X-Naver-Client-Id": "LFDIwR9DMgVRcfm0fCSh",
+    "X-Naver-Client-Secret": "4yvFIBoLrh"
 }
 
-def read_article(url):
-    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"}
-    webpage = req.get(url, headers)
-    soup = BeautifulSoup(webpage.text, "html.parser")
-    title = soup.select_one("#articleTitle").text.strip()
-    body = soup.select_one('#articleBodyContents').text.strip()
-    written_at = soup.select_one('#main_content > div.article_header > div.article_info > div > span.t11').text.split()[0]
-    return title, body, written_at
+def read_article(url, driver):
+    '''
+    - title: 기사제목
+    - body: 기사본문
+    - written_at: 기사 업로드 일자('YYYY.MM.DD.' 형식) 
+    '''
+    
+    driver.get(url)
+    title = driver.find_element(by=By.CSS_SELECTOR, value=".media_end_head_headline").text.strip() # "#articleTitle"
+    body = driver.find_element(by=By.CSS_SELECTOR, value="._article_body").text.strip() #'#articleBodyContents' 
+    try:
+        caption = driver.find_element(by=By.CSS_SELECTOR, value='.img_desc').text
+        body = body.replace(caption, '').strip() # removing image captions
+    except:
+        body = body.strip()
+        
+    written_at = driver.find_element(by=By.CSS_SELECTOR, value="#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span").text.split()[0] #'#main_content > div.article_header > div.article_info > div > span.t11'
+    category = driver.find_element(by=By.CSS_SELECTOR, value=".media_end_categorize_item").text.strip()
+    #title = driver.find_element_by_css_selector("#articleTitle").text
+    article = {'title':title, 'body':body, 'written_at':written_at, 'category':category}
+    return article
 
-def is_editorial(title):
-    if title.startswith('[<b>사설</b>]') or title.endswith('[<b>사설</b>]'): #or title.startswith('&lt;<b>사설</b>&gt')
-        # [사설] : 경향, 국민, 서울신문, 세계일보, 조선일보, 중앙일보, 한겨레, 한국일보, 매일경제, 한국경제, 서울경제
-        return True
-    return False
 
-def search_opinions(publisher='default', start=1, display=100):
+def search_editorials(start=1, display=100):
     """
     네이버 검색 API를 사용하여 기사들 link 추출
     - publisher: 신문사
-    - start: 검색 page index
-    - display: 노출 갯수
-
+    - start: 노출되는 기사 index로 display의 값에 따라 page_index가 달라짐. display=10이면 start=21 일 떼, page_index가 2
+    - display: 노출되는 수 
     연예/스포츠 섹션 뉴스는 제외
     """
-    publisher_ids = {'경향신문':1032,'국민일보':1005,'동아일보':1020,'서울신문':1081,'세계일보':1022, '조선일보':1023, \
-                    '중앙일보':1025, '한겨레':1028, '한국일보':1469, '매일경제':1009, '한국경제':1015, '서울경제':1011}
-    #publisher_id = publisher_ids[publisher]
-
-    naver_search_url = f"https://openapi.naver.com/v1/search/news.json?query=사설&start={start}&display={display}"
+    naver_search_url = f"https://openapi.naver.com/v1/search/news.json?query=사설&start={start}&sort=sim&display={display}"
     res = req.get(naver_search_url, headers=headers)
     news_links = []
+    #news_titles= []
+    publishers = []
     if res.status_code == 200:
         res = res.json()
         items = res['items']
         for item in items:
-            link = item['link']
+            naver_link = item['link']
+            original_link = item['originallink']
             title = item['title']
-            if 'news.naver.com' in link and is_editorial(title):
-                news_links.append(link)
+            if 'news.naver.com' in naver_link and '[<b>사설</b>]' in title:
+                news_links.append(naver_link)
+                #news_titles.append(title)
+                if 'chosun' in original_link:
+                    publisher = '조선일보'
+                elif 'donga' in original_link:
+                    publisher = '동아일보'
+                elif 'hani' in original_link:
+                    publisher = '한겨레'
+                elif 'joongang' in original_link:
+                    publisher = '중앙일보'
+                elif 'khan' in original_link:
+                    publisher = '경향신문'
+                else:
+                    publisher = ''
+                publishers.append(publisher)
         time.sleep(0.15)
     else:
         print(res.status_code)
 
-    return news_links
+    return news_links, publishers
 
-links = search_opinions()
-print(links)
+def create_train_data():
+    chrome_options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    news_links, publishers = search_editorials()
+    bodies, newspapers= [], []
+    for link, source in zip(news_links, publishers):
+        article = read_article(link, driver)
+        if article['category'] == '오피니언':
+            bodies.append(article['body'])
+            newspapers.append(source)
+    df = pd.DataFrame({'body': bodies, 'newspaper':newspapers})
+    driver.close()
+    return df
 
-for link in links:
-    title, body, written_at = read_article(link)
-    print(title)
+df = create_train_data()
+print(df.head())
